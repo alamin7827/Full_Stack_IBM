c569593d2665b8db214e0d47f707124c84f5c51b
alamin akhtar <alaminakhtar8@gmail.com>
2025-02-21 19:05:16 +0530

    ClassWork 9

---
+++ Day_9/Class_Work/index.js
@@ -0,0 +1,53 @@
+// {
+//     let a =10;
+//     console.log(10);
+// }
+// console.log(a);
+
+//Arithematoc opetaor
+//+,-,*,/,%,**
+
+//Assignment operator
+// =,+=,-=,*=,/=,**=
+
+//Comparestion operator
+// ==,!=,!==,===,<,>,<=,>=
+
+//Logical operatr
+//and ,or , not
+
+// let a =10, b="10";
+
+//console.log(a+=a);
+// if(a===+b){
+//     console.log("a is equal to b");
+// }
+// else{
+//     console.log("a is not equal to b");
+// }
+
+// let a =10;
+// let b= 20;
+
+// const myFunction=(a,b)=>{
+//     console.log(a+b);
+// }
+// console.log(myFunction(a,b));
+// // myFunction(a,b);
+
+// console.log(name)
+
+// var name ="alexzandar"
+// myName()
+// console.log(a);
+
+// function myName(){
+//     let a= 10
+//     console.log(a)
+// }
+
+// let d ="   ";
+// console.log(d.length)
+// let d =20 , b=+'30';
+let d =20 , b='30';
+console.log(d+b);
