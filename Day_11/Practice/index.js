9962fb11bf0fad64eaa454abd967358b152719a5
alamin akhtar <alaminakhtar8@gmail.com>
2025-02-25 22:41:25 +0530

    Practice

---
+++ Day_11/Practice/index.js
@@ -0,0 +1,137 @@
+// // let a=prompt("Enter the value of a");
+// // let b=prompt("Enter the value of b");
+// let a=50;
+// let b=60;
+// let sum=a+b;
+// let diff=a-b;
+// let div=a/b;
+// let mull=a*b;
+// console.log("The sum two value is:",sum);
+// console.log("alamin akhtar");
+// console.log("bera");
+// console.log(a*b);
+// const year=2024;
+// //year=2026;
+// console.log("current year is:",year);
+// var age=23;
+// var cgpa=8.9;
+// var nums1=1;
+// var nums2=2;
+// var sum1=nums1+nums2;
+// console.log("The sum of two numbers is:",sum1);
+// let name="TONY STARK";
+// let name2='I am a "Engineer"';
+// console.log("1st string is:",name);
+// console.log("2nd string is:",name2);
+// let firstname="tony";
+// let secondname="bera"
+// let fulname=firstname+" "+secondname;
+// console.log(fulname);
+// console.log(fulname.length);
+// console.log("first character of a string is:",firstname[firstname.length-1]);
+// let color=prompt("Choose your color:");
+// if(color=="red"){
+//     console.log("stop");
+// }
+// else if(color=="yellow"){
+//     console.log("slow down");
+// }else{
+//     console.log("go");
+// }
+// //switch case
+// let c=prompt("Choose your color:");
+// switch(c){
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "yellow":
+//         console.log("slow down");
+//         break;
+//     case "green":
+//         console.log("go");
+//         break;
+//     default:
+//         console.log("Broken light");
+// }
+// alert("something is wrong");
+// let n=prompt("Enter your age:");
+// if(n==18){
+//     alert("you are adult");
+// }else if(n<18){
+//     alert("you are not aligible");
+// }else{
+//     alert("you are eligible");
+// }
+// let marks=[99,85,93,76,62];
+// let names=["adam","bob","catyln"];
+// let info=["aman",25,5.6];
+// let n=prompt("Write your number:");
+// n=parseInt(n);
+// for(let i=n;i<=n*10;i=i+n){
+//     console.log(i);
+// }
+// for(let i=1;i<=n;i++){
+//     for(let j=1;j<=n;j++){
+//         console.log(i);
+//     }
+// }
+// let i=1;
+// while(i<=n){
+//     console.log(i);
+//     i++;
+// }
+
+// fovarite movie
+// const fav="avatar";
+// let guess=prompt("guess my favourite movie");
+// while((guess!=fav)&&guess!="quite"){
+//     guess=prompt("Wrong guess please try again");
+// }
+// if(guess==fav){
+//     console.log("Congrats");
+// }
+// else{
+//     console.log("you quite");
+// }
+
+// let i=1;
+// while(i<=5){
+//     if(i==3){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+// console.log("we us");
+// let num=[[1,2,3,4,5],[6,7,8,9,10]];
+// for(let i=0;i<num.length;i++){
+//     console.log(`List#${i}`);
+//     for(let j=0;j<num[0].length;j++){
+//         console.log(num[i][j]);
+//     }
+// }
+
+//for of loop...........
+let fruits=["mango","banana","apple","litchi"];
+for(fruit of fruits){
+    console.log(fruit);
+}
+for(char of "subratabera"){
+    console.log(char);
+}
+let heroes=[["ironman","spiderman"],["superman","wonder woman"]];
+for(list of heroes){
+    for(hero of list){
+        console.log(hero);
+    }
+}
+
+
+
+
+
+
+
+
+
+
