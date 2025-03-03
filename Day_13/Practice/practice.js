fc00d3841c4fdc86b9b5fdfbc2308dd1a3153b27
alamin akhtar <alaminakhtar8@gmail.com>
2025-03-03 23:04:56 +0530

    Practice

---
+++ Day_13/Practice/practice.js
@@ -0,0 +1,44 @@
+const student ={
+    name:"Shardha",
+    age :23,
+    marks:94.4,
+    city :"Delhi"
+};
+
+//object of object
+const classinfo ={
+    aman:{
+        grade:"A+",
+        city :"Delhi"
+    },
+    shardha :{
+        grade :"A",
+        cuty :"Pune"
+    },
+    sumon :{
+        grade :"O",
+        city :"Mumbai"
+    }
+};
+
+//array of object
+const boysinfo=[
+    {
+        name:"Aman",
+        grade:"A+",
+        city:"Delhi"
+    },
+    {
+        name:"shardha",
+        grade:"A",
+        city:"Pune"
+    },
+    {
+        name:"Karan",
+        grade:"O",
+        city:"Mumbai" 
+    }
+
+];
+
+let number =Math.floor(Math.random() *10)+1;
